48df8d8c60701c9eb51dd011909c296a2b61ff56
Rokon007 <rokon@example.com>
2026-01-17 09:08:37 +0600

    added a commit

---
+++ src/Layout/Mainlayout.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Outlet } from "react-router";
+import Navbar from "../components/navbar";
+import Footer from "../components/Footer";
+
+const MainLayout = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Main Content */}
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
